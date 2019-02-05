992191a497334b2aff8e783ea5da11d188a0f05d
GaryWork <gary.armstrong@ororavisual.com>
2019-02-05 14:12:22 -0600

    Rework Program.cs for demonstration.

---
+++ src/ArtFactory.Domain/JobStatus.cs
@@ -0,0 +1,14 @@
+﻿namespace ArtFactory.Domain
+{
+  public enum JobStatus
+  {
+    Waiting = 1,
+    InProgress = 2,
+    Completed = 3,
+    Failed = 4,
+    Aborting = 5,
+    Aborted = 6,
+    Deployed = 7,
+    Suspended = 8
+  }
+}
---
+++ src/ArtFactory.Experimental/ProofArgs.cs
@@ -0,0 +1,38 @@
+﻿namespace ArtFactory.Experimental
+{
+  using System.Collections.Generic;
+  using System.Linq;
+  using ArtFactory.Experimental.JobTicket;
+
+  public class ProofArgs
+  {
+    private static List<string> NonStringFieldNames { get; }
+
+    public int DocumentId { get; set; }
+    public Dictionary<string, string> Fields { get; set; }
+
+    static ProofArgs()
+    {
+      NonStringFieldNames = new List<string>
+      {
+        "plantId", "_greenPriceBox", "_prop65"
+      };
+    }
+  
+    public Customization[] GetStringCustomizations()
+    {
+      return Fields
+        .Where(f => !NonStringFieldNames.Contains(f.Key))
+        .Select(f => new Customization {m_Name = f.Key, m_Expression = f.Value, m_IOType = "R", m_Type = "VAR"})
+        .ToArray();
+    }
+
+    public Customization[] GetNonStringCustomizations()
+    {
+      return Fields
+        .Where(f => NonStringFieldNames.Contains(f.Key))
+        .Select(f => new Customization {m_Name = f.Key, m_Expression = f.Value, m_IOType = "R", m_Type = "VAR"})
+        .ToArray();
+    }
+  }
+}
